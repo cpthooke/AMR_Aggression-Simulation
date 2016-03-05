c7ed34e043fc4fef37a7e979157bfb23f5404f39
cpthooke <stevenhooke@hotmail.co.uk>
2016-03-05 18:42:19 +0000

    05/03 Assignment Update (NEEDS CHECKING IN LABS)
    Code commented and tidied.

--- ASSIGNMENT.py
+++ ASSIGNMENT.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import rospy
-import cv2
-import numpy
-from sensor_msgs.msg import Image
-from cv_bridge import CvBridge, CvBridgeError
-from std_msgs.msg import Float32
-from geometry_msgs.msg import Twist # This is the message type the robot uses for velocities
+import rospy #Allows for python.
+import cv2 #Allows for OpenCV.
+import numpy #Allows for python mathematics.
+from sensor_msgs.msg import Image #Imports ability to read images.
+from cv_bridge import CvBridge, CvBridgeError #Allows for conversion.
+from std_msgs.msg import Float32 #Allows for ROS messages/data types.
+from geometry_msgs.msg import Twist #Imports angular posing.
             
 wheel_radius = 0.35
 robot_radius = 0.35
@@ -15,80 +15,80 @@ robot_radius = 0.35
 class Assignment():
     def __init__(self): #self. = this.
         rospy.loginfo("Starting info: ")
-        cv2.namedWindow("Left Window", 1)
+        cv2.namedWindow("Left Window", 1) #Defines the two window halves (left & right).
         cv2.namedWindow("Right Window", 1)
         cv2.startWindowThread()
-        self.bridge = CvBridge()        
-        self.wheel_sub = rospy.Subscriber("/wheel_vel_left", Float32, self.callback)
-        self.pub = rospy.Publisher("/cmd_vel", Twist, queue_size = 1)
+        self.bridge = CvBridge() #Makes a bridge that converts ROS images to OpenCV images.
+		
+#########################For the REAL ROBOT#########################								
+        self.wheel_sub = rospy.Subscriber("/wheel_vel", Float32, self.callback) #Subscribes to the wheel velocity of the robot (reads any information as it is published from the requested source).
+        self.pub = rospy.Publisher("/cmd_vel", Twist, queue_size = 1) #Publishes requested information to method.
         self.image_sub = rospy.Subscriber("/camera/rgb/image_raw",
-                                          Image, self.callback)
-
-    def callback(self, data):
-        #(v, a) = self.forward_kinematics(data.data, 0.0)
-        #t = Twist()
-             
-        #t.linear.x = v
-        #t.angular.z = a
-             
-        #self.pub.publish(t)
-								
+                                          Image, self.callback) #Subscribes to the image data from the robot.
+#########################For the SIMULATION#########################								
+        #self.wheel_sub = rospy.Subscriber("turtlebot_1/wheel_vel", Float32, self.callback) #Following lines are similar to those above but are based around the Turtlebot simulation.
+        #self.pub = rospy.Publisher("turtlebot_1/cmd_vel", Twist, queue_size = 1)
+        #self.image_sub = rospy.Subscriber("turtlebot_1/camera/rgb/image_raw",
+        #                                  Image, self.callback)
+    def callback(self, data):						
         try:
             cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
         except CvBridgeError, e:
             print e
-        image_size = cv_image.shape
-        print image_size[0]
-        print image_size[1]
+        image_size = cv_image.shape #Defines image output size dependent on window size (defined below).
+        #print image_size[0]
+        #print image_size[1]
       
-        #b, g, r = cv2.split(cv_image)
-        l = cv_image[:, 0:320]
+        l = cv_image[:, 0:320] #Defines window size of left and right hand window.
         r = cv_image[:, 320:640]				
-        #px = img[100,100]
-        #print px #[157 166 200]
         
 #########################LEFT Hand Window#########################								
-        bgr_thresh_l = cv2.inRange(l,
-                                numpy.array((0, 0, 0)),
-                                numpy.array((0, 255, 0)))
+	####################BGR Model####################								
+        #bgr_thresh_l = cv2.inRange(l,
+        #                        numpy.array((0, 0, 0)),
+        #                        numpy.array((0, 255, 0))) #Searches for any green colour
+        #
         
-        hsv_img_l = cv2.cvtColor(l, cv2.COLOR_BGR2HSV)
-        
+        #bgr_contours_l, hierachy_l = cv2.findContours(bgr_thresh_l.copy(),
+        #                        cv2.RETR_TREE,
+        #                        cv2.CHAIN_APPROX_SIMPLE) #Defines the areas that are contained in the search threshold.
+		
+	####################HSV Model####################								
+        hsv_img_l = cv2.cvtColor(l, cv2.COLOR_BGR2HSV) #Converts image colour to hue/saturation/value(lightness) colour model.
+		
         hsv_thresh_l = cv2.inRange(hsv_img_l,
                                 numpy.array((60, 200, 0)),
-                                numpy.array((130, 255, 255)))
+                                numpy.array((130, 255, 255))) #Searches for any green colour.
                  
-        bgr_contours_l, hierachy_l = cv2.findContours(bgr_thresh_l.copy(),
-                                cv2.RETR_TREE,
-                                cv2.CHAIN_APPROX_SIMPLE)
-
         hsv_contours_l, hierachy_l = cv2.findContours(hsv_thresh_l.copy(),
                                 cv2.RETR_TREE,
-                                cv2.CHAIN_APPROX_SIMPLE)
+                                cv2.CHAIN_APPROX_SIMPLE) #Defines the areas that are contained in the search threshold.
         for c in hsv_contours_l:
             a = cv2.contourArea(c)
             if a > 100.0:
-                cv2.drawContours(l, c, -1, (255, 0, 0),3)
-                w=0.4
+                cv2.drawContours(l, c, -1, (255, 0, 0),3) #Draws dots of the colours found in the search threshold.
+                speed = 0.4
                 t = Twist()
-                t.linear.x, t.angular.z = self.forward_kinematics(w,w+0.2)
-                self.pub.publish(t)
+                t.linear.x, t.angular.z = self.forward_kinematics(speed, speed + 0.2) #Increases the speed of the right wheel when a contour is found in the left hand side.
+                self.pub.publish(t) #Publishes the information.
                 
 #########################RIGHT Hand Window#########################
-        bgr_thresh_r = cv2.inRange(r,
-                                numpy.array((0, 0, 0)),
-                                numpy.array((0, 255, 0)))
+	####################BGR Model####################								
+        #bgr_thresh_r = cv2.inRange(r,
+        #                        numpy.array((0, 0, 0)),
+        #                        numpy.array((0, 255, 0)))
         
+        #bgr_contours_r, hierachy_r = cv2.findContours(bgr_thresh_r.copy(),
+        #                        cv2.RETR_TREE,
+        #                        cv2.CHAIN_APPROX_SIMPLE)
+
+	####################HSV Model####################								
         hsv_img_r = cv2.cvtColor(r, cv2.COLOR_BGR2HSV)
         
         hsv_thresh_r = cv2.inRange(hsv_img_r,
                                 numpy.array((60, 200, 0)),
                                 numpy.array((130, 255, 255)))
                  
-        bgr_contours_r, hierachy_r = cv2.findContours(bgr_thresh_r.copy(),
-                                cv2.RETR_TREE,
-                                cv2.CHAIN_APPROX_SIMPLE)
-
         hsv_contours_r, hierachy_r = cv2.findContours(hsv_thresh_r.copy(),
                                 cv2.RETR_TREE,
                                 cv2.CHAIN_APPROX_SIMPLE)
@@ -97,58 +97,34 @@ class Assignment():
             a = cv2.contourArea(c)
             if a > 100.0:
                 cv2.drawContours(r, c, -1, (255, 0, 0),3)
-                w=0.4
+                speed = 0.4
                 t = Twist()
-                t.linear.x, t.angular.z = self.forward_kinematics(w+0.2,w)
+                t.linear.x, t.angular.z = self.forward_kinematics(speed + 0.2, speed) #Increases the speed of the left wheel when a contour is found in the right hand side.
                 self.pub.publish(t)
-                               
-
-        
-        #IplImage* frame1 = 
         
         print '===='
         cv2.imshow("Left Window", l)
-        cv2.imshow("Right Window", r)
+        cv2.imshow("Right Window", r) #Opens the two window halves.
 								
 
-    def listener(self):
-        rospy.init_node('listener', anonymous=True)
-        rospy.Subscriber()
-        rospy.Spin()
+    #def listener(self):
+    #    rospy.init_node('listener', anonymous=True)
+    #    rospy.Subscriber()
+    #    rospy.Spin()
 		
-    # computing the forward kinematics for a differential drive
-    def forward_kinematics(self, w_r, w_l):
+    def forward_kinematics(self, w_r, w_l): #Moves the robot.
         c_l = wheel_radius * w_l
         c_r = wheel_radius * w_r
-        v = (c_l + c_r) / 2
-        a = (c_l - c_r) / robot_radius
+        v = (c_l + c_r) / 2 #Defines the linear velocity.
+        a = (c_l - c_r) / robot_radius #Defines the angular velocity
         return (v, a)
 
-
-	# computing the inverse kinematics for a differential drive
-	#def inverse_kinematics(self, v, a):
-	#	c_l = v + (robot_radius * a) / 2
-	#	c_r = v - (robot_radius * a) / 2
-	#	w_l = c_l / wheel_radius
-	#	w_r = c_r / wheel_radius
-	#	return (w_l, w_r)
-
-
-	# inverse kinematics from a Twist message (This is what a ROS robot has to do)
-    def inverse_kinematics_from_twist(self, t):
-        return self.inverse_kinematics(t.linear.x, t.angular.z)
-
-	#(w_l, w_r) = inverse_kinematics(0.0, 1.0)
-	#print "w_l = %f,\tw_r = %f" % (w_l, w_r)
-
-	#(v, a) = forward_kinematics(w_l, w_r)
-	#print "v = %f,\ta = %f" % (v, a)
+	#def inverse_kinematics_from_twist(self, t):
+    #    return self.inverse_kinematics(t.linear.x, t.angular.z)
 
 
 if __name__ == '__main__':
-    rospy.init_node("ASSIGNMENT")
-    this = Assignment()
-    #cv = ()
-    #cv.send_velocities() # Calling the function
-    rospy.spin()
-    cv2.destroyAllWindows()
+    rospy.init_node("ASSIGNMENT") #Initialises the program.
+    this = Assignment() #Initialises the class.
+    #rospy.spin()
+    cv2.destroyAllWindows() #Closes all windows upon exit.
